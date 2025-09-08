4d566c4bebb5aa1fafbe8ad6cb7e59347a390c79
Jan Moloto <janmoloto8@gmail.com>
2025-09-08 10:50:34 +0200

    Adjust port to 8080

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ import presenceRoutes from './routes/presenceRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ----------------------------
 // Middleware
